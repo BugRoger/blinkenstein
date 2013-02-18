78883be4517be76337854f6122aebe048ffc8b85
Michael Schmidt <michael.j.schmidt@gmail.com>
2013-02-18 23:34:37 +0100

    refactoring again. this still sucks

--- lib/eve/skill_queue.rb
+++ lib/eve/skill_queue.rb
@@ -1,66 +1,68 @@
 require "eve/base"
 
 class Eve::SkillQueue < Eve::Base
-    attr_reader :expire_time
+  include Blinkenstein::Logging
 
-    def initialize
-      @expire_time = Time.now - 1
-    end
+  attr_reader :expire_time
 
-    def hours_left
-      refresh
+  def initialize
+    @expire_time = Time.now - 1
+  end
 
-      return ((end_time - current_time) * 24).to_i if end_time 
-      return  0 if paused? || empty?
-      return -1 if blocked?
-      -1
-    end
+  def hours_left
+    refresh
 
-    def paused?
-      return false if blocked?
+    return ((end_time - current_time) * 24).to_i if end_time 
+    return  0 if paused? || empty?
+    return -1 if blocked?
+    -1
+  end
 
-      last_skill.fetch("endTime", false) == "" 
-    end
+  def paused?
+    return false if blocked?
 
-    def blocked?
-      @response.fetch("eveapi", {}).fetch("error", false)
-    end
+    last_skill.fetch("endTime", false) == "" 
+  end
 
-    def empty?
-      return false if blocked?
-      
-      last_skill.empty?
-    end
+  def blocked?
+    @response.fetch("eveapi", {}).fetch("error", false)
+  end
 
-    def last_skill
-      @last_skill ||= Array[@response.fetch("eveapi", {}).fetch("result", {}).fetch("rowset", {}).fetch("row", {})].flatten.last
-    end
+  def empty?
+    return false if blocked?
 
-    def current_time 
-      parse_date(@response.fetch("eveapi", {}).fetch("currentTime", {}))
-    end
+    last_skill.empty?
+  end
 
-    def cached_until
-      parse_date(@response.fetch("eveapi", {}).fetch("cachedUntil", {}))
-    end
+  def last_skill
+    @last_skill ||= Array[@response.fetch("eveapi", {}).fetch("result", {}).fetch("rowset", {}).fetch("row", {})].flatten.last
+  end
 
-    def end_time
-      parse_date(last_skill.fetch("endTime", ""))
-    end
+  def current_time 
+    parse_date(@response.fetch("eveapi", {}).fetch("currentTime", {}))
+  end
 
-    def update_cache
-      if current_time && cached_until
-        @expire_time = Time.now + ((cached_until - current_time) * 24 * 60 * 60).to_i
-      else
-        @expire_time = Time.now + 60 
-      end
-    end
+  def cached_until
+    parse_date(@response.fetch("eveapi", {}).fetch("cachedUntil", {}))
+  end
 
-    def refresh 
-      return if @response && Time.now < @expire_time 
+  def end_time
+    parse_date(last_skill.fetch("endTime", ""))
+  end
 
-      @response = self.class.get('/char/SkillQueue.xml.aspx', query: query)
-      update_cache 
+  def update_cache
+    if current_time && cached_until
+      @expire_time = Time.now + ((cached_until - current_time) * 24 * 60 * 60).to_i
+    else
+      @expire_time = Time.now + 60 
     end
   end
+
+  def refresh 
+    return if @response && Time.now < @expire_time 
+    logger.info "Updating Skillqueue from Eve-API"
+    @response = self.class.get('/char/SkillQueue.xml.aspx', query: query)
+    update_cache 
+  end
+end
 
